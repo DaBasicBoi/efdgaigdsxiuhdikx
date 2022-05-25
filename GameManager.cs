3371662edcc1abdb7c9c26f6f567e2ea4c99d2dd
DaBasicBoi <105464321+DaBasicBoi@users.noreply.github.com>
2022-05-25 17:03:44 -0400

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+   public bool gameHasEnded = false;
+   public GameObject gameManager;
+   public Uded
+   public class Uded {
+
+        EndGame ()
+        {
+            if (gameHasEnded == false)
+            {
+            gameHasEnded = true;
+            Uded = SetActive(true);
+            Restart();
+            }
+        }
+
+        void Restart ()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
